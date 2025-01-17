90db7aed0fdfe8210fcfab1708d49fa25870c8f2
Vinh <vinhpham123.np@gmail.com>
2025-01-18 05:05:50 +0700

    implement buying (without player) system

--- scripts/MarketManager.gd
+++ scripts/MarketManager.gd
@@ -3,17 +3,22 @@ extends Node
 # You can use this to access the singleton instance, for example: MarketManager.cash
 var cash = 1000.0
 
+var stock_list = [
+	{"name": "AAA", "initial_price": 10.0, "growth_rate": 0.2, "volatility": 0.5},
+	{"name": "BBB", "initial_price": 25.0, "growth_rate": 0.1, "volatility": 0.2},
+]
+
 @onready var buy_popup = get_node("BuyPopup")
-var selected_stock
+@onready var stock_info_dropdown = get_node("ButtonGroup/StockInfoDropdown")
 
-func _ready():
-	#buy_popup.hide()
-	#buy_popup.connect("buy_confirmed", _on_buy_confirmed)
+
+var selected_stock = null
+
+func _ready():	
+	# Spawn stock
+	for stock_data in stock_list:
+		spawn_stock_bubble(stock_data.name, stock_data.initial_price, stock_data.growth_rate, stock_data.volatility)
 	print("MarketManager ready!") # Just to confirm it's loaded
-
-func _on_buy_confirmed(quantity):
-	if selected_stock != null:
-		buy_stock(selected_stock, quantity)
 
 # --- Stock Management ---
 func spawn_stock_bubble(name, initial_price, growth_rate, volatility):
@@ -42,4 +47,16 @@ func _on_buy_pressed() -> void:
 
 func _on_sell_pressed() -> void:
 	print("Sell pressed")
+	pass # Replace with function body.
+
+
+func _on_stock_info_dropdown_item_selected(index: int) -> void:
+	selected_stock = index
+	pass # Replace with function body.
+
+
+func _on_buy_popup_buy_confirmed(quantity: Variant) -> void:
+	print(selected_stock, quantity)
+	if selected_stock != null:
+		buy_stock(selected_stock, quantity)
 	pass # Replace with function body.
