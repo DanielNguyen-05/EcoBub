cbd8943a580b5341913e8e72671b19c946513317
Vinh <vinhpham123.np@gmail.com>
2025-01-19 00:29:29 +0700

    implement core algorithm

--- scripts/MarketManager.gd
+++ scripts/MarketManager.gd
@@ -4,15 +4,18 @@ extends Node2D
 var cash = 1000.0
 
 var stock_list = [
-	{"name": "AAA", "current_price": 10.0, "growth_rate": 0.2, "volatility": 0.5},
-	{"name": "BBB", "current_price": 25.0, "growth_rate": 0.1, "volatility": 0.2},
+	{"name": "AAA", "current_price": 10.0, "growth_rate": 0.2, "volatility": 0.8, "later_price": 10.0},
+	{"name": "BBB", "current_price": 25.0, "growth_rate": 0.1, "volatility": 0.9, "later_price": 25.0},
 ]
 
 var owned_stocks = []
 
 var news_list = []
-var current_news_index = 0
+var current_day = 0
 
+var game_duration = 300.0
+var game_over = false
+var panic_meter = 0.0
 
 @onready var buy_popup = get_node("../Main/World/PopupGroup/BuyPopup")
 @onready var sell_popup = get_node("../Main/World/PopupGroup/SellPopup")
@@ -26,12 +29,9 @@ var current_news_index = 0
 var selected_stock = null
 
 func _ready():	
-	# Spawn stock
-	for stock_data in stock_list:
-		spawn_stock_bubble(stock_data.name, stock_data.current_price, stock_data.growth_rate, stock_data.volatility)
 	# Load news from CSV
 	load_news_from_csv("res://assets/news.csv")
-	news_ticker.update_news(news_list[current_news_index].title, news_list[current_news_index].content)
+	# news_ticker.update_news(news_list[current_news_index].title, news_list[current_news_index].content)
 	print("MarketManager ready!") # Just to confirm it's loaded
 	return
 	
@@ -59,10 +59,6 @@ func add_news(title, content, stock, impact) -> void:
 	pass
 
 
-# --- Stock Management ---
-func spawn_stock_bubble(name, current_price, growth_rate, volatility):
-	pass # Implement later
-
 # --- Player Actions ---
 func buy_stock(stockid: int, shares_quantity: int):
 	cash -= shares_quantity * stock_list[stockid]["current_price"]
@@ -83,6 +79,9 @@ func buy_stock(stockid: int, shares_quantity: int):
 	
 	sell_popup.update_owned(owned_stocks)
 	
+	stock_list[stockid]["growth_rate"] += 0.08
+	stock_list[stockid]["later_price"] += 0.12 * shares_quantity
+
 	return # Implement later
 
 func sell_stock(stockid: int, shares_quantity: int):
@@ -90,6 +89,9 @@ func sell_stock(stockid: int, shares_quantity: int):
 	
 	var stock_name = stock_list[stockid]["name"]
 	
+	stock_list[stockid]["growth_rate"] -= 0.05
+	stock_list[stockid]["later_price"] -= 0.2 * shares_quantity
+
 	for owned in owned_stocks:
 		if owned["name"] == stock_name:
 			owned["shares"] -= shares_quantity
@@ -102,33 +104,46 @@ func sell_stock(stockid: int, shares_quantity: int):
 
 # --- Other Game Logic ---
 func next_day() -> void:
-	print("Old stock prices: ", stock_list)
-	print("Old news: ", news_list[current_news_index].title)
-	update_stock_prices(news_list[current_news_index].stock, news_list[current_news_index].impact) # Update stock prices
-	print("New stock prices: ", stock_list)
-	print("New news: ", news_list[current_news_index].title)
-	current_news_index += 1
-	if current_news_index >= news_list.size():
-		current_news_index = 0
-	news_ticker.update_news(news_list[current_news_index].title, news_list[current_news_index].content) # Update news ticker
-	# Trigger a market crash if the conditions are met
+	#draw graph(current price, later price)
+	if current_day - 1 >= 0:
+		affect_later_stock_price(news_list[(current_day - 1) % news_list.size()].stock, news_list[(current_day - 1) % news_list.size()].impact)
+	current_day += 1
+	update_stock_prices()
+
 	pass
 	
 func get_owned_stocks():
 	print(owned_stocks)
 	return owned_stocks
 
-func update_stock_prices(stockId, delta):
-	stock_list[stockId].current_price += stock_list[stockId].growth_rate * delta
-	stock_list[stockId].current_price += RandomNumberGenerator.new().randf_range(-stock_list[stockId].volatility, stock_list[stockId].volatility)
-	pass # Implement later
-	
+func affect_later_stock_price(stockID, impact):
+	# TODO: Implement this function to affect the stock price more realistically
+	stock_list[stockID]["later_price"] += impact
+	if impact > 2:
+		stock_list[stockID]["volatility"] += 0.2
+
+	pass
+
+func update_stock_prices():
+	for stock in stock_list:
+		stock.current_price += stock.growth_rate * RandomNumberGenerator.new().randf_range(-stock.volatility, stock.volatility)
+		stock.current_price += RandomNumberGenerator.new().randf_range(-stock.volatility, stock.volatility)
+		panic_meter += stock.volatility
+		stock.later_price = stock.current_price + stock.growth_rate * RandomNumberGenerator.new().randf_range(-stock.volatility, stock.volatility)
+	if panic_meter > 10:
+		trigger_market_crash()
+	pass
+
 func _process(delta: float) -> void:
-	trigger_market_crash()
+	game_duration -= delta
+	if game_duration <= 0:
+		game_over = true
+	if game_over:
+		trigger_market_crash()
 
 func trigger_market_crash():
-	if Input.is_action_just_pressed("ui_right"):
-		get_tree().change_scene_to_file("res://scenes/EndScreen.tscn")
+	# TODO: Play crash effect
+	get_tree().change_scene_to_file("res://scenes/EndScreen.tscn")
 	pass # Implement later
 
 
